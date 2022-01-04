47bf031de4eee4835281e48ecbce2bf993bd7ef5
chuno <chunoc@gmail.com>
2022-01-04 13:44:58 +0800

    export device

--- anroidmonitor12.sh
+++ anroidmonitor12.sh
@@ -1,5 +1,7 @@
 export LEANCLOUD_API_SERVER=http://192.168.124.48:7000
 cd /home/ubuntu/boxtest
+sudo adb kill-server
+sudo adb start-server
 sleep 30
 source ~/jncloudvenv3/bin/activate
 nohup python androidmonitor.py>>amonitor.out  2>&1 &
